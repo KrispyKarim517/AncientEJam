ef9c6f9edfc984071b956d4d185d2a1304feb195
JosefBertolini <39634656+JosefBertolini@users.noreply.github.com>
2020-08-22 06:30:04 -0700

    kinda some stuff

---
+++ AncientEJam/Assets/Scripts/CustomAnimatorStateBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomAnimatorStateBehavior : StateMachineBehaviour
+{
+
+    [SerializeField] Animator animator;
+
+    Switch switch_class = null;
+
+    public void SetSwitch(Switch m_switch)
+    {
+        switch_class = m_switch;
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).Equals(stateInfo))
+        {
+            switch_class.AnimationEnd();
+        }
+    }
+}
---
+++ AncientEJam/Assets/Scripts/Switch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Switch : MonoBehaviour
+{
+    /// <summary>
+    /// Set Real Way to get Player later
+    /// </summary>
+    public GameObject player;
+
+    [SerializeField] Target target = null;
+
+    private Animator animator = null;
+
+    public string desired_equipment = "ERROR_MUST_OVERRIDE";
+
+    private bool state_toggle = false;
+
+    StateMachineBehaviour animator_state_machine;
+
+    public abstract void Activate();
+    public void AnimationEnd()
+    {
+        // Won't compile until player script is written
+        //player.Equip();
+    }
+
+
+}
+
