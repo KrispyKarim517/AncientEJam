dfe6e33e23f75b9631d2115acb7c4f6a65eddae0
something999 <wongny1@uci.edu>
2020-08-23 17:57:05 -0700

    Added a prototype version of the UI

---
+++ AncientEJam/Assets/Scripts/script_KeyMapping.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+    AUTHOR: Nichole Wong
+    UNITY VERSION: 2020.1.0f1
+    LAST MODIFIED: 8/23/2020
+    
+    A script that allows the user to map actions to different keys
+*/
+public class script_KeyMapping : MonoBehaviour
+{
+    void Update()
+    {
+        foreach (KeyCode k in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(k))
+            {
+                Debug.Log(k);
+            }
+        }
+    }
+
+}
---
+++ AncientEJam/Assets/Scripts/script_SwitchTools.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    AUTHOR: Nichole Wong
+    UNITY VERSION: 2020.1.0f1
+    LAST MODIFIED: 8/22/2020
+    
+    A script that allows the user to cycle through tools upon pressing a button
+    By default, the cycling button is TAB
+*/
+public class script_SwitchTools : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
