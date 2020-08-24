d8cd54f3cbf3c042ec3c3a558046e50969a9e26d
nuclearMonkeys <lyndond@uci.edu>
2020-08-23 21:27:52 -0700

    Added AudioManager

--- AncientEJam/Assets/Scripts/script_CameraFollowPlayer.cs
+++ AncientEJam/Assets/Scripts/script_CameraFollowPlayer.cs
@@ -2,50 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Referenced from this video: 
-// https://www.youtube.com/watch?v=hT-dyANbHDY&list=PLdgLYFdStKu2RTKC6f9LhIC2Xb6eJfRag&index=3&t=0s
-
 public class script_CameraFollowPlayer : MonoBehaviour
 {
-    script_PlayerController player;
-    public bool followPlayer = true;
-    Vector3 mousePos;
+    private script_PlayerController player;
 
     private void Start() 
     {
-        player = script_PlayerController.instance;
-    }
-    
-    private void Update()
-    {
-        if (Input.GetKey(KeyCode.LeftShift)) 
-        {
-            followPlayer = false;
-            player.canMove = false;
-        }
-        else 
-        {
-            followPlayer = true;
-            player.canMove = true;
-        }
-        if (followPlayer)
-            camFollowPlayer();
-        else
-            lookAhead();
+        
     }
 
-    private void camFollowPlayer() 
+    private void Update() 
     {
-        Vector3 newPos = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
-        this.transform.position = newPos;
-    }
-
-    private void lookAhead() 
-    {
-        Vector3 camPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        camPos.z = 10;
-        Vector3 dir = camPos - this.transform.position;
-        transform.Translate(dir * 2 * Time.deltaTime);
 
     }
 }
---
+++ AncientEJam/Assets/Scripts/test.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        script_AudioManager.instance.PlaySound("boomer_going");
+    }
+}
