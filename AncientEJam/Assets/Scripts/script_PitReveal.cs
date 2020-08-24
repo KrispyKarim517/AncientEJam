b6588e172a9956ca4779193c7f2cf5cc7ad3b3f3
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2020-08-24 04:17:59 -0700

    Level 2 Pit Traps

---
+++ AncientEJam/Assets/Scripts/script_PitReveal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_PitReveal : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Renderer>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
