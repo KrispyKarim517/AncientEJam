8d0a0940261215a42856f94c7d1b9c13ac9dea33
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2020-08-24 00:04:15 -0700

    Camera Follow Complete

--- AncientEJam/Assets/Scripts/script_CameraFollowPlayer.cs
+++ AncientEJam/Assets/Scripts/script_CameraFollowPlayer.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class script_CameraFollowPlayer : MonoBehaviour
 {
-    private script_PlayerController player;
+    public GameObject player1;
+    public float smoothTime = 0.3f;
+    public float maxSpeed = 3f;
+    private Vector3 velocity = Vector3.zero;
 
-    private void Start() 
+    private float xOffset;
+    private float zOffset;
+
+    void Start()
     {
-        
+        xOffset = transform.position.x;
+        zOffset = transform.position.z;
     }
 
-    private void Update() 
+    void FixedUpdate()
     {
+        Vector3 target = new Vector3(player1.transform.position.x + xOffset, transform.position.y, player1.transform.position.z + zOffset);
 
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime, maxSpeed);
     }
 }
