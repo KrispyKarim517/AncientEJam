68b00187c120b10fab5ea97b6042fbcbeab097e6
KrispyKarim517 <39722911+KrispyKarim517@users.noreply.github.com>
2020-08-24 02:32:13 -0700

    Stuff

---
+++ AncientEJam/Assets/Scripts/script_LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class script_LoadScene : MonoBehaviour
+{
+    void onTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //Debug.Log("Next Scene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
